763be7f358098893e619db2221e7900ab2b5dc2e
Todd W Crone <tcrone@newrelic.com>
2023-09-20 10:45:36 -0400

    13-12

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 #[test]
-fn iterator_sum() {
+fn iterator_demonstration() {
     let v1 = vec![1, 2, 3];
 
-    let v1_iter = v1.iter();
+    let mut v1_iter = v1.iter();
 
-    let total: i32 = v1_iter.sum();
-
-    assert_eq!(total, 6);
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
 }
