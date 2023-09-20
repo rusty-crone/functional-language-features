1779bfd45accf8cc71397a9a0802d8fd0487f64a
Todd W Crone <tcrone@newrelic.com>
2023-09-20 10:41:51 -0400

    13-10, 13-11

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,11 @@ struct Rectangle {
 }
 
 fn main() {
-    let mut list = [
-        Rectangle { width: 10, height: 1 },
-        Rectangle { width: 3, height: 5 },
-        Rectangle { width: 7, height: 12 },
-    ];
+    let v1 = vec![1, 2, 3];
 
-    let mut num_sort_operations = 0;
-    list.sort_by_key(|r| {
-        num_sort_operations += 1;
-        r.width
-    });
-    println!(
-        "{:#?}, sorted in {num_sort_operations} operations",
-        list
-    );
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {val}");
+    }
 }
