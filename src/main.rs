606ab3598ad4af2fde51b0f29a65b24aba38f732
Todd W Crone <tcrone@newrelic.com>
2023-09-20 10:47:16 -0400

    13-14

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,7 @@
 // }
 
 fn main() {
-    let v1 = vec![1, 2, 3];
+    let v1: Vec<i32> = vec![1, 2, 3];
 
-    let v1_iter = v1.iter();
-
-    for val in v1_iter {
-        println!("Got: {val}");
-    }
+    v1.iter().map(|x| x + 1);
 }
